3f1332b06507ee9226777abe35799ffeff9abb5a
Mohamed Anwer <mohammed.ahmed.anwer@gmail.com>
2015-10-25 03:21:51 +0200

    adding fonts

--- install.sh
+++ install.sh
@@ -7,10 +7,14 @@ cp -r "$PWD/terminator" ~/.config
 cp "$PWD/.i3status.conf" ~
 cp "$PWD/.gitconfig" ~
 cp "$PWD/.vimrc" ~
+cp "$PWD/fonts" ~/.local/share
+
+# Caching new fonts
+fc-cache -fv
 
 # Installing important apps
 # TODO: Complete this list
-echo $password | sudo -S pacman --noconfirm -S git tig zsh gvim i3 feh dmenu lxappearance nm-applet terminator
+echo $password | sudo -S pacman --noconfirm -S git tig zsh gvim i3 feh dmenu lxappearance nm-applet terminator conky
 
 # Install vundle
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
