9fa93446b08f2ac9c1d4ffd2b4b36f951d001678
Mohamed Anwer <mohammed.ahmed.anwer@gmail.com>
2015-10-25 18:04:08 +0200

    Adding coloring for battery

--- .i3/conky/bat-stat.sh
+++ .i3/conky/bat-stat.sh
@@ -1,7 +1,17 @@
-stat=$(acpi | awk {'print $3'})
-if [[ "$stat" == "Charging," ]];
-then
+# icon
+stat=$(acpi | awk {'print $3'} | sed 's/\,//g')
+
+if [[ "$stat" == "Charging" ]]; then
   echo ""
 else
   echo ""
+fi
+
+# color
+percentage=$(acpi | awk {'print $4'} | sed 's/\%,//g')
+
+if [[ "$percentage" -lt "15" ]]; then
+	echo "red"
+else
+	echo "#7f9f7f"
 fi
